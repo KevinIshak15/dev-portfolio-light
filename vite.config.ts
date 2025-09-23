8335f9d38ebbaddf19ed747aa4de468d63f8fa9c
KevinIshak15 <56769270+KevinIshak15@users.noreply.github.com>
2025-09-23 12:54:05 -0400

    Fix GitHub Pages - 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/dev-portfolio-light/",
+  base: mode === 'production' ? "/dev-portfolio-light/" : "/",
   server: {
     host: "::",
     port: 8080,
